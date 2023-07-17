86ea1a4921488293f0354718c8591d0f6e6ae7bb
Leonardo Luis <leoluisbsantos16@gmail.com>
2023-07-17 17:21:47 -0300

    adcionando no git

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+function trocacor(cor){
+let circulo = document.querySelector(".circulo")
+console.log(circulo)
+circulo.style.background = cor
+
+}
+
+function trocaImgem(Imagem){
+let Img = document.querySelector(".iphone")
+Img.src = Imagem
+
+}
